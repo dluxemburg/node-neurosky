731cc25386a1f956bc1c0d811a72e7cacb4e1db4
Daniel Luxemburg <daniel.luxemburg@gmail.com>
2014-03-30 14:47:46 -0400

    update module name in example

--- example/app.js
+++ example/app.js
@@ -1,12 +1,12 @@
-var nodeThinkGear = require('../lib');
+var neurosky = require('../lib');
 
-var tgClient = nodeThinkGear.createClient({
-	appName:'NodeThinkGear',
+var client = neurosky.createClient({
+	appName:'NodeNeuroSky',
 	appKey:'0fc4141b4b45c675cc8d3a765b8d71c5bde9390'
 });
 
-tgClient.on('data',function(data){
+client.on('data',function(data){
 	console.log(data);
 });
 
-tgClient.connect();
+client.connect();
