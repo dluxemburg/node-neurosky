771c9d69ca2a3b54d552506ea55e6700b429e530
Daniel Luxemburg <daniel.luxemburg@gmail.com>
2014-03-30 14:50:26 -0400

    update module name in test

--- test/test.js
+++ test/test.js
@@ -1,36 +1,36 @@
-var nodeNeuroSky = require('../lib')
+var neurosky = require('../lib')
 var assert = require("assert")
 
-describe('nodeNeuroSky', function(){
+describe('neurosky', function(){
   describe('#createClient()', function(){
 
     it('should raise an exception when appName is not supplied', function(){
       var probe
       try{
-        nodeNeuroSky.createClient()
+        neurosky.createClient()
       }catch(e){
         probe = e
       }
-      assert.equal(probe.message, "Must specify appName");
+      assert.equal(probe.message, "Must specify appName")
     })
 
     it('should raise an exception when appKey is not supplied', function(){
       var probe
       try{
-        nodeNeuroSky.createClient({
+        neurosky.createClient({
           appName:'MyGreatApp'
         })
       }catch(e){
         probe = e
       }
-      assert.equal(probe.message, "Must specify appKey");
+      assert.equal(probe.message, "Must specify appKey")
     })
 
     it('should creat a ThinkGearClient client instance', function(){
-      assert.equal(nodeNeuroSky.createClient({
+      assert.equal(neurosky.createClient({
         appName:'MyGreatApp',
         appKey:'123456789'
-      }).constructor, nodeNeuroSky.ThinkGearClient);
+      }).constructor, neurosky.NeuroSkyClient)
     })
   })
 })
