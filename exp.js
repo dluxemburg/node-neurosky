2322f22ceed7d7e04f2b8f956e1b3329a9a95ef9
KudosAbhay <kudosabhay@gmail.com>
2019-03-25 14:04:57 +0530

    change to datetime fomat

--- exp.js
+++ exp.js
@@ -1,4 +1,5 @@
 var neurosky = require('./lib')
+var dateFormat = require('dateformat');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 /* 
@@ -14,7 +15,7 @@ var payload = {};
 console.log(userFolderName);
 
 const csvWriter = createCsvWriter({
-	path: '../../Reports/' + userFolderName + '/' + userFolderName + '_EEG.csv',
+	path: '../../Reports/' + userFolderName + '/raw/' + userFolderName + '_EEG.csv',
 	header: [
         { id: 'time', title: 'DATETIME' },
         { id: 'delta', title: 'DELTA' },
@@ -27,7 +28,8 @@ const csvWriter = createCsvWriter({
         { id: 'hg', title: 'HIGH GAMMA' },
         {id: 'attention', title: 'ATTENTION'},
         { id: 'relaxed', title: 'RELAXED' },
-		{ id: 'stressed', title: 'STRESSED' }
+        { id: 'stressed', title: 'STRESSED' },
+        { id: 'blinkStrength', title: 'BLINK'}
 	]
 });
 
@@ -36,6 +38,10 @@ var client = neurosky.createClient({
 	appName:'NodeNeuroSky',
 	appKey:'0fc4141b4b45c675cc8d3a765b8d71c5bde9390'
 })
+
+
+client.connect();
+
 
 function saveRecord(records) {
     csvWriter.writeRecords(records) // returns a promise
@@ -47,78 +53,101 @@ function saveRecord(records) {
     process.on('unhandledRejection', up => { console.log(up) })
 }
 
-function newRecord(datetime, data, incoming_var) {
-    if(incoming_var === 'eSense'){
-        return [
-            {
-                time: datetime,
-                delta: data.eegPower.delta,
-                theta: data.eegPower.theta,
-                la: data.eegPower.lowAlpha,
-                ha: data.eegPower.highAlpha,
-                lb: data.eegPower.lowBeta,
-                hb: data.eegPower.highBeta,
-                lg: data.eegPower.lowGamma,
-                hg: data.eegPower.highGamma,
-                attention: data.eSense.attention,
-                relaxed: data.eSense.meditation,
-                stressed: 100 - data.eSense.meditation
-            }
-        ];    
-    }else{
-        return [
-            {
-                time: datetime,
-                delta: 0,
-                theta: 0,
-                la: 0,
-                ha: 0,
-                lb: 0,
-                hb: 0,
-                lg: 0,
-                hg: 0,
-                attention: 0,
-                relaxed: 0,
-                stressed: 0
-            }
-        ];    
-    }
 
-
+function newEEGRecord(datetime, data){
+    return [
+        {
+            time: datetime,
+            delta: data.eegPower.delta,
+            theta: data.eegPower.theta,
+            la: data.eegPower.lowAlpha,
+            ha: data.eegPower.highAlpha,
+            lb: data.eegPower.lowBeta,
+            hb: data.eegPower.highBeta,
+            lg: data.eegPower.lowGamma,
+            hg: data.eegPower.highGamma,
+            attention: data.eSense.attention,
+            relaxed: data.eSense.meditation,
+            stressed: 100 - data.eSense.meditation,
+            blinkStrength: 0
+        }
+    ];    
 }
+
+
+function newBlankRecord(datetime){
+    return [
+        {
+            time: datetime,
+            delta: 0,
+            theta: 0,
+            la: 0,
+            ha: 0,
+            lb: 0,
+            hb: 0,
+            lg: 0,
+            hg: 0,
+            attention: 0,
+            relaxed: 0,
+            stressed: 0,
+            blinkStrength: 0
+        }
+    ];
+}
+
+
+function newBlinkRecord(datetime, data){
+    return [
+        {
+            time: datetime,
+            delta: 0,
+            theta: 0,
+            la: 0,
+            ha: 0,
+            lb: 0,
+            hb: 0,
+            lg: 0,
+            hg: 0,
+            attention: 0,
+            relaxed: 0,
+            stressed: 0,
+            blinkStrength: data.blinkStrength
+        }
+    ];
+}
+
 
 client.on('data', function (data) {
 	// Capture Date and Time
-	var datetime = new Date();
-	console.log(datetime)
-	console.log(data)
+    var now = dateFormat(new Date(), "yyyy-mm-dd hh:MM:ss");
+	console.log(now);
+	console.log(data);
 
     if(data.eSense){
         // Store incoming stream of data as a record
-        let records = newRecord(datetime, data, 'eSense');
+        let records = newEEGRecord(now, data);
+        payload = data;
 
         // Write record to CSV file
         saveRecord(records);
     }else if(data.blinkStrength){
         // Store incoming stream of data as a record
-        let records = newRecord(datetime, data, 'eSense');
+        let records = newBlinkRecord(now, data);
 
         // Write record to CSV file
         saveRecord(records);
     }else{
         // Store incoming stream of data as a record
-        let records = newRecord(datetime, data, 'status');
+        let records = newBlankRecord(now);
 
         // Write record to CSV file
         saveRecord(records);
+        sleep(1000);
     }
-
 	console.log("\n");
 });
+
 
 client.on('error', function (error) {
     console.log(error);
 });
-
-
-client.connect();
